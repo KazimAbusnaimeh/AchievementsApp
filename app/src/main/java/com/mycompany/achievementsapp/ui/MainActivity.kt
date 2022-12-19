0f180860773addc1ed2896c882badfa6d4b61953
kazim <NAME>
2022-12-19 10:02:59 +0300

    Add Navigation Bar: Add navigation bar to the project and costume to match the design

--- app/src/main/java/com/mycompany/achievementsapp/ui/MainActivity.kt
+++ app/src/main/java/com/mycompany/achievementsapp/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mycompany.achievementsapp.R
 import com.mycompany.achievementsapp.databinding.ActivityMainBinding
 import com.mycompany.achievementsapp.ui.viewmodels.AchievementsViewModel
@@ -22,10 +23,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController=navHostFragment.findNavController()
+        navController = navHostFragment.findNavController()
+        setSupportActionBar(binding.tbMain)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/mycompany/achievementsapp/ui/fragments/AchievementsFragment.kt
+++ app/src/main/java/com/mycompany/achievementsapp/ui/fragments/AchievementsFragment.kt
@@ -1,19 +1,13 @@
 package com.mycompany.achievementsapp.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.mycompany.achievementsapp.R
-import com.mycompany.achievementsapp.data.Achievements
 import com.mycompany.achievementsapp.databinding.FragmentAchievementBinding
 import com.mycompany.achievementsapp.ui.viewmodels.AchievementsViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,10 +17,9 @@ class AchievementsFragment : Fragment(R.layout.fragment_achievement) {
 
 
     val viewModel: AchievementsViewModel by hiltNavGraphViewModels(R.id.nav_graph)
-    private lateinit var iconsList:List<ImageView>
+    private lateinit var iconsList: List<ImageView>
 
 
-    private val TAG = "AchievementsFragment"
     lateinit var binding: FragmentAchievementBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,7 +27,6 @@ class AchievementsFragment : Fragment(R.layout.fragment_achievement) {
 
         initLists()
         bindData()
-        changeActivationState()
         onItemClick()
 
     }
@@ -58,35 +50,31 @@ class AchievementsFragment : Fragment(R.layout.fragment_achievement) {
                 record10 = it.data[1].records[3]
                 record11 = it.data[1].records[4]
                 record12 = it.data[1].records[5]
-            })
-        }
-    }
-
-    private fun changeActivationState() {
-        binding.apply {
-            viewModel.setRecordsList()
-            viewModel.record.observe(viewLifecycleOwner, Observer {
                 val recordsList =
-                    listOf(it[0],it[1],it[2],it[3],it[4],it[5],it[6],it[7],it[8],it[9],it[10],it[11])
+                    listOf(record1, record2, record3, record4, record5, record6, record7,
+                        record8, record9, record10, record11, record12)
 
                 for ((index, record) in recordsList.withIndex()) {
-                        if (!record!!.active) {
-                            iconsList[index].isEnabled = false
-                            iconsList[index].alpha = 0.4f}
+                    if (!record!!.active) {
+                        iconsList[index].isEnabled = false
+                        iconsList[index].alpha = 0.4f
+                    }
                 }
             })
         }
     }
-    private fun onItemClick(){
+
+    private fun onItemClick() {
         binding.apply {
-            for((i,x) in iconsList.withIndex()){
+            for ((i) in iconsList.withIndex()) {
                 iconsList[i].setOnClickListener {
                     findNavController().navigate(R.id.action_achievementsFragment_to_racesFragment)
                 }
             }
-    }}
+        }
+    }
 
-    private fun initLists(){
+    private fun initLists() {
         binding.apply {
             iconsList =
                 listOf(ivIcon1, ivIcon2, ivIcon3, ivIcon4, ivIcon5, ivIcon6, ivIcon7, ivIcon8,
