d9824373fca6d5e6168c2ba6dc02dc20dcd5c915
kazim <NAME>
2022-12-24 21:30:57 +0300

    Add Activation Binding:new binding method added to the Bindings.kt and connect to xml file

--- app/src/main/java/com/mycompany/achievementsapp/di/Bindings.kt
+++ app/src/main/java/com/mycompany/achievementsapp/di/Bindings.kt
@@ -1,9 +1,13 @@
 package com.mycompany.achievementsapp.di
 
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.mycompany.achievementsapp.R
+import com.mycompany.achievementsapp.databinding.ItemRecordBinding
+import com.mycompany.achievementsapp.datasource.models.Achievements
 
 @BindingAdapter("app:loadImageFromUri")
 fun loadImageFromUrl(imageView: ImageView,image: String?){
@@ -11,4 +15,12 @@ fun loadImageFromUrl(imageView: ImageView,image: String?){
         .load(image)
         .error(R.drawable.ic_launcher_background)
         .into(imageView)
+}
+
+@BindingAdapter("app:changeActivation")
+fun changeActivation(linearLayout: LinearLayout,records: Achievements.AchievementsData.Records){
+        if (!records.active) {
+            linearLayout.isEnabled = false
+            linearLayout.alpha = 0.4f
+    }
 }
