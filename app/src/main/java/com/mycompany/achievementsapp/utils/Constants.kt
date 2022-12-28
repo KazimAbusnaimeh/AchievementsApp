a38dd5c7a2681bfa6a7ae2ca797d5495acfa680b
kazim <NAME>
2022-12-28 08:07:11 +0300

    url issue solve

--- app/src/main/java/com/mycompany/achievementsapp/utils/Constants.kt
+++ app/src/main/java/com/mycompany/achievementsapp/utils/Constants.kt
@@ -12,6 +12,6 @@ object Constants {
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
-    const val BASE_URL = BuildConfig.API_KEY
+    const val BASE_URL = "https://786b905e-735c-4be6-adfb-949d5dadee32.mock.pstmn.io/"
     const val ACHIEVEMENT_FRAGMENT_TAG = "AchievementsFragment"
 }
